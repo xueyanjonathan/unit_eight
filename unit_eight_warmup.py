27b5379923a64bb127c662294135d8c22d205ce7
Jonathan <jonathan@iMac9.local>
2018-11-12 08:24:58 -0500

    Create function to convert fahrenheit to celsius and create a button to iniate the funciton

--- unit_eight_warmup.py
+++ unit_eight_warmup.py
@@ -3,6 +3,13 @@ import tkinter
 root = tkinter.Tk()
 root.title("Temperature Converter")
 
+
+def f_to_c():
+    f_value = int(fahrenheitValue.get())
+    c_value = 5 / 9 * (f_value - 32)
+    celsiusValue.set(str(c_value))
+
+
 fahrenheitValue = tkinter.StringVar()
 celsiusValue = tkinter.StringVar()
 
@@ -17,5 +24,8 @@ celsiusLabel.grid(row=2, column=1)
 
 celsiusAnswer = tkinter.Label(root, textvariable=celsiusValue)
 celsiusAnswer.grid(row=2, column=2)
+
+convertButton = tkinter.Button(root, text="Convert", command=f_to_c)
+convertButton.grid(row=3, column=1)
 
 root.mainloop()
